5d4783256f27e0341fd3fde4fca6c49fad2a85ce
takuo-maeda <cafeacb@olivesheep34.sakura.ne.jp>
2021-03-07 18:28:01 +0900

    メッセージの表示

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,17 @@
 class RoomsController < ApplicationController
-  def index
+  def new
+    @room = Room.new
   end
+
+  #３月５日（金） Routing Error
+   # No route matches [POST] "/rooms/new"
+  #  フロント実装問１０番
+  #  def
+  #  end
+  # ままになっていた
+  # ルートエラーは新規チャットルームのcreateボタン
+  #route errorは次に⇨<%=form_with model: @room, local: true do |f|%>の＠roomからコントローラー
+  #rooms_controllerの２行目から３行目に（上の分コード）「@room = Room.new」が記述されていなかった
   def create
     # binding pry
     @room = Room.new(room_params)
