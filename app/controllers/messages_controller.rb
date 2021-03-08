dfdafc8708a97bf94de31d9eb6b34bd3023d5525
takuo-maeda <cafeacb@olivesheep34.sakura.ne.jp>
2021-03-08 23:58:45 +0900

    画像のみでもメッセージを送信可能になるバリデーション設定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,20 +1,22 @@
 class MessagesController < ApplicationController
-  def index
+
+ def index
     @message = Message.new
     @room = Room.find(params[:room_id])
     @messages = @room.messages.includes(:user)
+    # binding.pry
   end
 
   def create
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
-  if @message.save
-    redirect_to room_messages_path(@room)    
-  else
-    @messages = @room.messages.includes(:user)
-    render :index
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      @messages = @room.messages.includes(:user)
+      render :index
+    end
   end
- end
 
   private
 
